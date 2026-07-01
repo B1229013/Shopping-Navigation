c7b583ae11668e3d503c0d3f9b45d3acf14ff540
choushing <shingchou868@gmail.com>
2026-07-01 17:22:54 +0800

    Initial Commit

---
+++ APPNAV_ios/APPNAV_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPNAV_iosApp.swift
+//  APPNAV_ios
+//
+//  Created by 周庠 on 2026/7/1.
+//
+
+import SwiftUI
+
+@main
+struct APPNAV_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
